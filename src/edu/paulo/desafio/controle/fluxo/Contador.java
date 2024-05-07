c32bf2253fc96747a05016b9a798b1a3b055f4e3
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2024-05-06 22:07:13 -0300

    inserindo comentarios

--- src/edu/paulo/desafio/controle/fluxo/Contador.java
+++ src/edu/paulo/desafio/controle/fluxo/Contador.java
@@ -5,22 +5,23 @@ import java.util.Scanner;
 public class Contador {
 
 	public static void main(String[] args) throws ParametrosInvalidosException {
-		try (var terminal = new Scanner(System.in)) {
-			System.out.println("Digite o primeiro parâmetro: ");
-			int parametroUm = terminal.nextInt();
-			System.out.println("Digite o segundo parâmetro: ");
-			int parametroDois = terminal.nextInt();
-			
-			try {
-			    contar(parametroUm, parametroDois);
-			} catch (ParametrosInvalidosException e) {
-			    System.err.println(e.getMessage());
+			try (Scanner terminal = new Scanner(System.in)) {
+				System.out.println("Digite o primeiro parâmetro: ");
+				int parametroUm = terminal.nextInt();
+				System.out.println("Digite o segundo parâmetro: ");
+				int parametroDois = terminal.nextInt();
+				// metodo para contar os parametros
+				try {
+				    contar(parametroUm, parametroDois);
+				} catch (ParametrosInvalidosException e) {
+				    System.err.println(e.getMessage());
+				}
 			}
-		}
-		
+			
 		
 	}
-
+// Criou a exceção a ser tratada
+	
 		 static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 		        if (parametroUm > parametroDois) {
 		            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
