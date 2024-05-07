3932eab616dc38fa4ef30c1e6a38fefdd4c128b8
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2024-05-06 21:59:21 -0300

    Desafio Contador de Fluxo

---
+++ src/edu/paulo/desafio/controle/fluxo/Contador.java
@@ -0,0 +1,38 @@
+package edu.paulo.desafio.controle.fluxo;
+
+import java.util.Scanner;
+
+public class Contador {
+
+	public static void main(String[] args) throws ParametrosInvalidosException {
+		try (var terminal = new Scanner(System.in)) {
+			System.out.println("Digite o primeiro parâmetro: ");
+			int parametroUm = terminal.nextInt();
+			System.out.println("Digite o segundo parâmetro: ");
+			int parametroDois = terminal.nextInt();
+			
+			try {
+			    contar(parametroUm, parametroDois);
+			} catch (ParametrosInvalidosException e) {
+			    System.err.println(e.getMessage());
+			}
+		}
+		
+		
+	}
+
+		 static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		        if (parametroUm > parametroDois) {
+		            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+		        }
+
+		        int contagem = parametroDois - parametroUm;
+
+		        for (int num = 0; num <= contagem; num++) {
+		            System.out.println("Imprimindo o número " + num);
+		        }
+		    
+	
+
+	}
+}
---
+++ src/edu/paulo/desafio/controle/fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+package edu.paulo.desafio.controle.fluxo;
+
+public class ParametrosInvalidosException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message) {
+	    super(message);
+	  }
+
+}
